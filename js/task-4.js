d648ef1f59c1af20de15fc34ecb17b73fe2d3af5
VikMelanchenko <63492650+VikMelanchenko@users.noreply.github.com>
2020-09-28 01:58:49 +0300

    исправляет ошибку в скрипте

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,19 @@
-// создаем переменную в которой будет хранится текущее значение счетчика
+const btnCounter = {
+  btn1: document.querySelector("[data-action='increment']"),
+  btn2: document.querySelector("[data-action='decrement']"),
+};
 let counterValue = 0;
-// создаем функции increment и decrement для увеличения и уменьшения значения счетчика
-
-const countBtn = {
-  btn1: (btnDecrement = document.querySelector("[data-action='decrement']")),
-  btn2: (btnIncrement = document.querySelector("[data-action='increment']")),
-  span: (spanCount = document.querySelector("#value")),
-  div: (divCounter = document.querySelector("#counter")),
-};
-
-const decrement = () => {
-  counterValue -= 1;
-  document.getElementById("value").innerHTML = counterValue;
-};
 
 const increment = () => {
   counterValue += 1;
-  document.getElementById("value").innerHTML = counterValue;
+  document.querySelector("#value").textContent = counterValue;
 };
 
-// добавляем слушатели кликов на кнопки, вызовы функций и обновление интерфейса
-countBtn.btn1.addEventListener("click", decrement);
-countBtn.btn2.addEventListener("click", increment);
+btnCounter.btn1.addEventListener("click", increment);
+
+const decrement = () => {
+  counterValue -= 1;
+  document.querySelector("#value").textContent = counterValue;
+};
+
+btnCounter.btn2.addEventListener("click", decrement);
